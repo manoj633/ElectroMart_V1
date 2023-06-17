24c9eea7100fc961a460814156bfda2fc5e8e822
Manoj <manoj09633@gmail.com>
2023-06-17 22:54:03 +0530

    Added star based rating system

--- frontend/src/components/product.jsx
+++ frontend/src/components/product.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
+
+import Rating from "./Rating";
+
 const Product = ({ product }) => {
   return (
     <Card className="my-3 p-3 rounded">
@@ -13,6 +16,12 @@ const Product = ({ product }) => {
             <strong>{product.name}</strong>
           </Card.Title>
         </Link>
+        <Card.Text as="div">
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+          />
+        </Card.Text>
         <Card.Text as="h3">${product.price}</Card.Text>
       </Card.Body>
     </Card>
