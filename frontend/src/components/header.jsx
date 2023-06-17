652c808f0aed789bf8358ea09a218caf48aa84c7
Manoj <manoj09633@gmail.com>
2023-06-17 20:19:46 +0530

    using logo and custom bootstrap

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -1,24 +1,34 @@
-import React from 'react'
-import {Navbar, Nav, Container} from 'react-bootstrap';
-import {FaShoppingCart, FaUser} from 'react-icons/fa'; 
+import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { FaShoppingCart, FaUser } from "react-icons/fa";
+import logo from "../assets/logo.png";
 
 const header = () => {
   return (
     <header>
-        <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
-            <Container>
-                <Navbar.Brand href="/">ElectroMart</Navbar.Brand>
-                <Navbar.Toggle aria-controls='basic-navbar-nav'/>
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className='ms-auto'>
-                        <Nav.Link href="/cart"><FaShoppingCart/>Cart</Nav.Link>
-                        <Nav.Link href="/login"><FaUser/>Sign In</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+      <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="/">
+            <img src={logo} alt="ElectroMart" className="navbar-logo" />
+            ElectroMart
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="/cart">
+                <FaShoppingCart />
+                Cart
+              </Nav.Link>
+              <Nav.Link href="/login">
+                <FaUser />
+                Sign In
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </header>
-  )
-}
+  );
+};
 
-export default header
+export default header;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+/*
+commenting default bootstrap and index.css
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+*/
+
+import "./assets/styles/index.css";
+import "./assets/styles/bootstrap.custom.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
