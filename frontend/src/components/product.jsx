bd254f5128846d85b09d8a862057c792964eef5c
Manoj <manoj09633@gmail.com>
2023-06-17 22:56:32 +0530

    overflowing text replaced by 3 dots

--- frontend/src/components/product.jsx
+++ frontend/src/components/product.jsx
@@ -12,7 +12,7 @@ const Product = ({ product }) => {
       </Link>
       <Card.Body>
         <Link to={`products/${product._id}`}>
-          <Card.Title as="div">
+          <Card.Title as="div" className="product-title">
             <strong>{product.name}</strong>
           </Card.Title>
         </Link>
