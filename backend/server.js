78bb6cd13e28cd5b2159aa7751bd85de721ca1c7
Manoj <manoj09633@gmail.com>
2023-06-18 11:13:20 +0530

    created backend api endpoints to fetch products and individual product

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,21 @@
 import express from "express";
+import products from "./data/products.js";
 const port = 5000;
 
 const app = express();
 app.get("/", (req, res) => {
   res.send("API is running");
+});
+
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+
+app.get("/api/product/:id", (req, res) => {
+  const product = products.find((p) => {
+    return p._id === req.params.id;
+  });
+  res.json(product);
 });
 
 app.listen(port, () => {
