ed3c9167cbbceaaaf840346c48b0c963c85323b3
Manoj <manoj09633@gmail.com>
2023-06-21 22:32:04 +0530

    Created routes and fetching data from mongoDB

--- backend/server.js
+++ backend/server.js
@@ -3,26 +3,19 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import connectDB from "./config/db.js";
-import products from "./data/products.js";
+import productRoutes from "./routes/productRoutes.js";
+
 const port = process.env.PORT || 5000;
 
 connectDB(); //Connect to MongoDB
 
 const app = express();
+
 app.get("/", (req, res) => {
   res.send("API is running");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-
-app.get("/api/product/:id", (req, res) => {
-  const product = products.find((p) => {
-    return p._id === req.params.id;
-  });
-  res.json(product);
-});
+app.use("/api/products", productRoutes);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
