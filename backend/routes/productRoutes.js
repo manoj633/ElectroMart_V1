fd07e7fbbcc76a7dfd160d0524eadc8effa521f1
Manoj <manoj09633@gmail.com>
2023-06-22 20:19:22 +0530

    Fixed individual not found issue

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -15,11 +15,10 @@ router.get(
 router.get(
   "/:id",
   asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.param.id);
+    const product = await Product.findById(req.params.id);
     if (product) {
-      res.json(product);
+      return res.json(product);
     }
-
     res.status(404).json({ message: "Product not found" });
   })
 );
