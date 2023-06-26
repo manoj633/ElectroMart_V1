77b2faf2f93ba2a88d0af83625075137f58818a3
Manoj <manoj09633@gmail.com>
2023-06-26 16:27:32 +0530

    cart item counter added to header

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { LinkContainer } from "react-router-bootstrap";
-import { Navbar, Nav, Container } from "react-bootstrap";
+import { Navbar, Nav, Container, Badge } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
 import logo from "../assets/logo.png";
+import { useSelector } from "react-redux";
 
-const header = () => {
+const Header = () => {
+  const { cartItems } = useSelector((state) => state.cart);
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
@@ -22,6 +24,13 @@ const header = () => {
                 <Nav.Link>
                   <FaShoppingCart />
                   Cart
+                  {cartItems.length > 0 ? (
+                    <Badge pill bg="success" style={{ marginLeft: "5px" }}>
+                      {cartItems.reduce((a, c) => a + c.qty, 0)}
+                    </Badge>
+                  ) : (
+                    ""
+                  )}
                 </Nav.Link>
               </LinkContainer>
               <LinkContainer to="/login">
@@ -38,4 +47,4 @@ const header = () => {
   );
 };
 
-export default header;
+export default Header;
