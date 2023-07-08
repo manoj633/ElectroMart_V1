44f7d6df0513e4433fd0ee8086bc1d9535f505d9
Manoj <manoj09633@gmail.com>
2023-07-08 20:05:35 +0530

    created admin route guard and separate headre tab for admin

---
+++ frontend/src/components/AdminRoute.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Outlet, Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+const AdminRoute = () => {
+  const userInfo = useSelector((state) => state.auth);
+  if (userInfo.userInfo && !userInfo.userInfo.isAdmin) {
+    // Redirect to the login page if userInfo is null or empty
+
+    return <Navigate to="/login" replace />;
+  }
+
+  // Render the Outlet component if userInfo exists
+  return <Outlet />;
+};
+
+export default AdminRoute;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -31,6 +31,8 @@ import PaymentScreen from "./screens/PaymentScreen";
 import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 import OrderScreen from "./screens/OrderScreen";
 import ProfileScreen from "./screens/ProfileScreen";
+import AdminRoute from "./components/AdminRoute";
+import OrderListScreen from "./screens/admin/OrderListScreen";
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
@@ -46,6 +48,9 @@ const router = createBrowserRouter(
         <Route path="/placeorder" element={<PlaceOrderScreen />} />
         <Route path="/order/:id" element={<OrderScreen />} />
         <Route path="/profile" element={<ProfileScreen />} />
+      </Route>
+      <Route path="" element={<AdminRoute />}>
+        <Route path="/admin/orderlist" element={<OrderListScreen />} />
       </Route>
     </Route>
   )
