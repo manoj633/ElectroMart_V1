66103e264e692ac3d515985bfca7f8ecadaf7f1d
Manoj <manoj09633@gmail.com>
2023-07-08 10:43:08 +0530

    order paid controller update and paypal setup

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -69,7 +69,26 @@ const getOrderById = asyncHandler(async (req, res) => {
 //@route    PUT /api/orders/:id/pay
 //@access   Private
 const updateOrderToPaid = asyncHandler(async (req, res) => {
-  res.send("update order to paid");
+  const order = await Order.findById(req.params.id);
+
+  if (order) {
+    order.isPaid = true;
+    order.paidAt = Date.now();
+    // comes from paypal
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_item: req.body.update_time,
+      email_address: req.body.payer.email_address,
+    };
+
+    const updatedOrder = await order.save();
+
+    res.status(200).json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error("Order not found");
+  }
 });
 
 //@desc     Update order to delivered
