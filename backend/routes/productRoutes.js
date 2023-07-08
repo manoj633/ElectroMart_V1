72fdda83037a9662542645061dbf6776f8073223
Manoj <manoj09633@gmail.com>
2023-07-08 23:12:40 +0530

    Created a static create product method

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,11 +4,12 @@ import Product from "../models/productModel.js";
 import {
   getProducts,
   getProductById,
+  createProduct,
 } from "../controllers/productController.js";
-
+import { protect, admin } from "../middleware/authMiddleware.js";
 const router = express.Router();
 
-router.route("/").get(getProducts);
+router.route("/").get(getProducts).post(protect, admin, createProduct);
 router.route("/:id").get(getProductById);
 
 export default router;
--- frontend/src/screens/admin/ProductListScreen.jsx
+++ frontend/src/screens/admin/ProductListScreen.jsx
@@ -4,12 +4,28 @@ import { Table, Button, Row, Col } from "react-bootstrap";
 import { FaTimes, FaEdit, FaTrash } from "react-icons/fa";
 import Message from "../../components/Message";
 import Loader from "../../components/Loader";
-import { useGetProductsQuery } from "../../slices/productsApiSlice";
+import {
+  useGetProductsQuery,
+  useCreateProductMutation,
+} from "../../slices/productsApiSlice";
+import { toast } from "react-toastify";
 const ProductListScreen = () => {
-  const { data: products, isLoading, error } = useGetProductsQuery();
+  const { data: products, isLoading, error, refetch } = useGetProductsQuery();
 
+  const [createProduct, { isLoading: loadingCreate }] =
+    useCreateProductMutation();
   const deleteHandler = () => {};
 
+  const createProductHandler = async () => {
+    if (window.confirm("Are you sure you want to create a new product?")) {
+      try {
+        await createProduct();
+        refetch();
+      } catch (err) {
+        toast.error(err?.data?.message || err?.error);
+      }
+    }
+  };
   return (
     <>
       <Row className="align-items-center">
@@ -17,12 +33,12 @@ const ProductListScreen = () => {
           <h1>Products</h1>
         </Col>
         <Col className="text-end">
-          <Button className="btn-sm m-3">
+          <Button className="btn-sm m-3" onClick={createProductHandler}>
             <FaEdit /> Create Product
           </Button>
         </Col>
       </Row>
-
+      {loadingCreate && <Loader />}
       {isLoading ? (
         <Loader />
       ) : error ? (
