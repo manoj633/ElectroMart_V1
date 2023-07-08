7312d068307814c7a916013c445c1b18e0db2a80
Manoj <manoj09633@gmail.com>
2023-07-08 09:48:42 +0530

    Made getOrderById from admin protected to protected route

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -13,7 +13,7 @@ const router = express.Router();
 
 router.route("/").post(protect, addOrderItems).get(protect, admin, getOrders);
 router.route("/mine").get(protect, getMyOrders);
-router.route("/:id").get(protect, admin, getOrderById);
+router.route("/:id").get(protect, getOrderById);
 router.route("/:id/pay").put(protect, admin, updateOrderToPaid);
 router.route("/:id/delivered").put(protect, admin, updateOrderToDelivered);
 
