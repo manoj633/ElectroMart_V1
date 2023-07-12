975aca5ed322479f6fc32a4cedb7a1938bb6841c
Manoj <manoj09633@gmail.com>
2023-07-12 17:30:12 +0530

    created controller methods for user

---
+++ frontend/src/screens/admin/userListScreen.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Row, Col, Form, Button } from "react-bootstrap";
+import Message from "../../components/Message";
+import Loader from "../../components/Loader";
+import FormContainer from "../../components/FormContainer";
+
+const userListScreen = () => {
+  return <div>userListScreen</div>;
+};
+
+export default userListScreen;
