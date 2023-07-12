9dacef12f629c878532491ecf86c240af7b58ea3
Manoj <manoj09633@gmail.com>
2023-07-12 19:22:57 +0530

    Create review backend controller and endpoint

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -7,6 +7,7 @@ import {
   createProduct,
   updateProduct,
   deleteProduct,
+  createProductReview,
 } from "../controllers/productController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 const router = express.Router();
@@ -17,5 +18,6 @@ router
   .get(getProductById)
   .put(protect, admin, updateProduct)
   .delete(protect, admin, deleteProduct);
+router.route("/:id/reviews").post(protect, createProductReview);
 
 export default router;
