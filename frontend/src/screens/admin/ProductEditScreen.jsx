c834bed8006f79097f0dfa60a9de4817a305dbb7
Manoj <manoj09633@gmail.com>
2023-07-12 17:02:19 +0530

    Created mutation and field to upload image

--- frontend/src/screens/admin/ProductEditScreen.jsx
+++ frontend/src/screens/admin/ProductEditScreen.jsx
@@ -8,6 +8,7 @@ import { toast } from "react-toastify";
 import {
   useUpdateProductMutation,
   useGetProductDetailsQuery,
+  useUploadProductImageMutation,
 } from "../../slices/productsApiSlice";
 
 const ProductEditScreen = () => {
@@ -30,6 +31,9 @@ const ProductEditScreen = () => {
 
   const [updateProduct, { isLoading: loadingUpdate }] =
     useUpdateProductMutation();
+
+  const [uploadProductImage, { isLoading: loadingUpoad }] =
+    useUploadProductImageMutation();
 
   const navigate = useNavigate();
 
@@ -66,6 +70,20 @@ const ProductEditScreen = () => {
       toast.error(err?.data?.message || err?.err);
     }
   };
+
+  const uploadFileHandler = async (e) => {
+    const formData = new FormData();
+    formData.append("image", e.target.files[0]);
+
+    try {
+      const res = await uploadProductImage(formData).unwrap();
+      toast.success(res.message);
+      setImage(res.image);
+    } catch (err) {
+      toast.error(err?.data?.message || err?.error);
+    }
+  };
+
   return (
     <>
       <Link to="/admin/productlist" className="btn btn-light my-3">
@@ -103,7 +121,20 @@ const ProductEditScreen = () => {
                 }}
               ></Form.Control>
             </Form.Group>
-            {/* Image input placeholder */}
+            <Form.Group controlId="image" className="my-3">
+              <Form.Label>Image</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter image url"
+                value={image}
+                onChange={(e) => setImage}
+              ></Form.Control>
+              <Form.Control
+                type="file"
+                label="Choose File"
+                onChange={uploadFileHandler}
+              ></Form.Control>
+            </Form.Group>
             <Form.Group controlId="brand" className="my-3">
               <Form.Label>Brand</Form.Label>
               <Form.Control
